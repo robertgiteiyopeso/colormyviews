e75c29e09d0e4d8a9008789af7caa678dfd3db67
Robert Gitei <robert.gitei@yopeso.com>
2022-07-18 12:32:14 +0300

    Makes boxes use random colros

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,12 +16,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun makeColored(view: View) {
+        val rnd = Random()
+        val color = Color.argb(255,
+            rnd.nextInt(256),
+            rnd.nextInt(256),
+            rnd.nextInt(256))
+
         when (view.id) {
-            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
-            R.id.box_two_text -> view.setBackgroundColor(Color.CYAN)
-            R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
-            R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
-            R.id.box_five_text -> view.setBackgroundColor(Color.GRAY)
+            R.id.box_one_text -> view.setBackgroundColor(color)
+            R.id.box_two_text -> view.setBackgroundColor(color)
+            R.id.box_three_text -> view.setBackgroundColor(color)
+            R.id.box_four_text -> view.setBackgroundColor(color)
+            R.id.box_five_text -> view.setBackgroundColor(color)
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
@@ -40,24 +47,25 @@ class MainActivity : AppCompatActivity() {
 
         val clickableViews = listOf<View>(
             boxOneText, boxTwoText, boxThreeText,
-            boxFourText, boxFiveText, rootConstraintLayout)
+            boxFourText, boxFiveText, rootConstraintLayout
+        )
 
         val buttons = listOf<Button>(redButton, greenButton, yellowButton)
 
-        for(item in clickableViews) {
+        for (item in clickableViews) {
             item.setOnClickListener { makeColored(it) }
         }
 
-        for(item in buttons) {
+        for (item in buttons) {
             item.setOnClickListener {
-                when(it.id) {
-                    R.id.red_button -> for(view in clickableViews - rootConstraintLayout) {
+                when (it.id) {
+                    R.id.red_button -> for (view in clickableViews - rootConstraintLayout) {
                         view.setBackgroundResource(R.color.my_red)
                     }
-                    R.id.yellow_button -> for(view in clickableViews - rootConstraintLayout) {
+                    R.id.yellow_button -> for (view in clickableViews - rootConstraintLayout) {
                         view.setBackgroundResource(R.color.my_yellow)
                     }
-                    else -> for(view in clickableViews - rootConstraintLayout) {
+                    else -> for (view in clickableViews - rootConstraintLayout) {
                         view.setBackgroundResource(R.color.my_green)
                     }
                 }
